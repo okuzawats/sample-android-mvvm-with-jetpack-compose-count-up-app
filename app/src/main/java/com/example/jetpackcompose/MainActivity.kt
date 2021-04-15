29ddb4d6bed16e551052a0de45bc03391869694a
okuzawats <okuzawats@gmail.com>
2021-04-16 08:36:15 +0900

    define CountUp

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,19 +3,42 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorld()
+            CountUp()
         }
     }
 }
 
 @Composable
-fun HelloWorld() {
-    Text(text = "Hello World!")
+fun CountUp() {
+    var count: Int by remember {
+        mutableStateOf(0)
+    }
+
+    Column {
+        Text(
+            text = count.toString()
+        )
+        Button(
+            onClick = {
+                count += 1
+            }
+        ) {
+            Text(
+                text = "Count Up!"
+            )
+        }
+    }
 }
